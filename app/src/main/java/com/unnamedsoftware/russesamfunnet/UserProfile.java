54749e9b6c194b0ae80d8506d76d35f50a844328
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-01-22 14:22:15 +0100

    Added (very uncompleted) user profile

---
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/UserProfile.java
@@ -0,0 +1,19 @@
+package com.unnamedsoftware.russesamfunnet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alexander Eilert Berg on 22.01.2018.
+ */
+
+public class UserProfile extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
