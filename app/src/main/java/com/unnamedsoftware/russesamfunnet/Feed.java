209dde66bfec180a03b646b660c4281c91b87140
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-01-22 10:25:24 +0100

    Added feed. Added Navigation bar. Added functionality to the navigation bar.

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/Feed.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/Feed.java
@@ -1,20 +1,113 @@
 package com.unnamedsoftware.russesamfunnet;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.ListViewAutoScrollHelper;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Feed extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ArrayAdapter<String> drawerAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private String activityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigationDrawer);
+        drawerList = (ListView) findViewById(R.id.navList);
+        activityTitle = getTitle().toString();
 
 
+        addDrawerItems();
+        setupDrawer();
 
 
 
 
     }
+
+    /**
+     * Populates the navigation menu
+     */
+    private void addDrawerItems()
+    {
+        String[] optionsArray = {"Profil", "Scoreboard", "Knute liste", "Instillinger", "Logout"};
+        drawerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, optionsArray);
+        drawerList.setAdapter(drawerAdapter);
+
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+        });
+    }
+
+    private void setupDrawer()
+    {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close)
+        {
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view)
+            {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        if (drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/unnamedsoftware/russesamfunnet/Login.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/Login.java
@@ -1,8 +1,35 @@
 package com.unnamedsoftware.russesamfunnet;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Alexander Eilert Berg on 19.01.2018.
  */
 
-public class Login {
+public class Login extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+            loginButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    startActivity(new Intent(Login.this, Feed.class));
+                }
+            });
+
+    }
+
+
 }
