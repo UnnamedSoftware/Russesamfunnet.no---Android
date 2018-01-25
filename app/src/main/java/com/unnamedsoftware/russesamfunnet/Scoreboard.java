6f2065ed9af067e1c5bccbbdd0d8c32ba8ae42f4
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-01-25 10:34:15 +0100

    Added back (functioning) button to scoreboard

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/Scoreboard.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/Scoreboard.java
@@ -2,43 +2,34 @@ package com.unnamedsoftware.russesamfunnet;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Alexander Eilert Berg on 22.01.2018.
  */
 
-public class Scoreboard extends Activity
+public class Scoreboard extends AppCompatActivity
 {
-    private Toolbar toolbar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
 
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
-
-
 
 }
