089eaf92e89fabc453df57301cfeccd65a64fcdc
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-01-25 10:36:50 +0100

    Added back (functioning) button and toolbar to user profile

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/UserProfile.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/UserProfile.java
@@ -2,6 +2,7 @@ package com.unnamedsoftware.russesamfunnet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Alexander Eilert Berg on 22.01.2018.
@@ -16,5 +17,18 @@ public class UserProfile extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
