cbecaa1c27642e4effbcf9b1f8605ea685eb5e3f
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-01-30 15:04:39 +0100

    Added recyclerView to feed

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/RecyclerView/FeedPost.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/RecyclerView/FeedPost.java
@@ -9,17 +9,36 @@ public class FeedPost
 {
     private String poster;
     private String post;
+    private String firstName;
+    private String surname;
+    private Integer russID;
 
-    public FeedPost(String poster,String post)
+    public FeedPost(String firstName, String surname, Integer russID,String post)
     {
-        this.poster = poster;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.russID = russID;
         this.post = post;
+        this.poster = firstName + " "  + surname;
     }
 
 
+    public String getFirstName(){return this.firstName;}
+    public String getSurname(){return this.surname;}
     public String getPoster(){return this.poster;}
     public String getPost(){return this.post;}
 
+    public Integer getRussID(){return this.russID;}
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+    public void setRussID(Integer russID){this.russID = russID;}
     public void setPoster(String poster){this.poster = poster;}
     public void setPost(String post){this.post  =post;}
 }
