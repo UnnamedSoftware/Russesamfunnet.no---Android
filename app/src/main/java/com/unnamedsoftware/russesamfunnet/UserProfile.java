996c1aec4b66df19270b08c23e2d9e7a4c627b3d
Hallvard-PC\HaIIvard <hydrogen324@gmail.com>
2018-02-05 14:01:03 +0100

    Gets user info from server and fills out the profile

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/UserProfile.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/UserProfile.java
@@ -3,6 +3,18 @@ package com.unnamedsoftware.russesamfunnet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+import com.unnamedsoftware.russesamfunnet.Entity.RussEntity;
+import com.unnamedsoftware.russesamfunnet.RecyclerView.FeedPost;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Alexander Eilert Berg on 22.01.2018.
@@ -10,7 +22,9 @@ import android.support.v7.widget.Toolbar;
 
 public class UserProfile extends AppCompatActivity
 {
-
+    JSONArray user = null;
+    String url = null;
+    RussEntity russ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +39,57 @@ public class UserProfile extends AppCompatActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        url = getString(R.string.url) + "userRuss?russId=1";
+        try {
+            getUserRuss();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void getUserRuss() throws IOException {
+        try {
+            new JSONParser(new JSONParser.OnPostExecute() {
+                @Override
+                public void onPostExecute(JSONArray jsonArray) {
+                    fillProfile(jsonArray);
+                }
+            }).execute(new URL(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fillProfile(JSONArray jsonArray)
+    {
+        try
+        {
+            user = jsonArray;
+
+            for(int i = 0; i < user.length(); i++)
+            {
+                JSONObject u = user.getJSONObject(i);
+
+                int russId = Integer.valueOf(u.getString("russId"));
+                String russStatus = u.getString("russStatus");
+                String firstName = u.getString("firstName");
+                String lastName = u.getString("lastName");
+                String email = u.getString("email");
+                String russPassword = u.getString("russPassword");
+                String russRole = u.getString("russRole");
+                int russYear = Integer.valueOf(u.getString("russYear"));
+
+                russ = new RussEntity(russId, russStatus, firstName, lastName, email, russPassword, russRole, russYear);
+
+                EditText userName = (EditText) findViewById(R.id.userName);
+                userName.setText(russ.getFirstName() + " " + russ.getLastName());
+            }
+
+        }catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
