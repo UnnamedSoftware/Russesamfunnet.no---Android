2ab5d7c6ff08c84ce0a655ac3223eabfe5433cff
Alexander Eilert Berg <zookthemagpie@hotmail.com>
2018-03-14 12:29:29 +0100

    Added more layout to hub Fixed toolbar back button

--- app/src/main/java/com/unnamedsoftware/russesamfunnet/GroupHub.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/GroupHub.java
@@ -1,6 +1,8 @@
 package com.unnamedsoftware.russesamfunnet;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * The group hub, contains a preview of the top collectors (top 3/5) and a link to the complete list.
@@ -11,4 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class GroupHub extends AppCompatActivity
 {
+    private String groupName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_hub);
+
+        this.groupName = getIntent().getExtras().getString("groupName");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Gruppe: " + groupName);
+
+        System.out.println(getSupportActionBar());
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
 }
--- app/src/main/java/com/unnamedsoftware/russesamfunnet/GroupList.java
+++ app/src/main/java/com/unnamedsoftware/russesamfunnet/GroupList.java
@@ -1,10 +1,13 @@
 package com.unnamedsoftware.russesamfunnet;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -17,6 +20,7 @@ import android.widget.Toast;
 public class GroupList extends AppCompatActivity
 {
     private FloatingActionButton floatingActionButton;
+    private Button dummyLinkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,10 +38,22 @@ public class GroupList extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
+                addNewGroup();
                 Toast.makeText(GroupList.this, "I've been clicked! :O", Toast.LENGTH_SHORT).show();
             }
         });
 
+        this.dummyLinkButton = findViewById(R.id.dummyLink);
+        dummyLinkButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent (GroupList.this, GroupHub.class);
+                intent.putExtra("groupName", "Data vg2");
+                startActivity(intent);
+            }
+        });
 
         System.out.println(getSupportActionBar());
         if(getSupportActionBar() != null)
@@ -46,4 +62,29 @@ public class GroupList extends AppCompatActivity
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
+
+    /**
+     * Displays the dialog for joining or creating new groups
+     */
+    private void addNewGroup()
+    {
+        final Dialog dialog = new Dialog(this);
+        dialog.setTitle ("Grupper");
+        dialog.setContentView(R.layout.group_list_dialog);
+
+        Button addNewGroupButton = dialog.findViewById(R.id.addNewGroupButton);
+        addNewGroupButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+
+
 }
